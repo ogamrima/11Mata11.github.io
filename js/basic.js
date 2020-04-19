f0da975265453c9dd9daa024f4f85feced163d4a
11Mata11 <mataskala.nej@gmail.com>
2020-04-19 20:10:18 +0200

    First

---
+++ js/basic.js
@@ -0,0 +1,13 @@
+function ready() {
+  var canv = document.getElementById("myCanvas");
+  // Update the path from the JavaScript code.
+  window.onload = function () {
+    canv.style.width = this.screen.width;
+    canv.style.height = screen.height;
+  };
+
+  window.onresize = function () {
+    canv.style.width = this.screen.width;
+    canv.style.height = screen.height;
+  };
+}
---
+++ js/main.js
@@ -0,0 +1,3 @@
+var myPath = new Path();
+myPath.strokeColor = "black";
+myPath.add(new Point(0, 0), new Point(1000, 1000));
